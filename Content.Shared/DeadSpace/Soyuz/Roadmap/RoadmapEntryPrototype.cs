c94b95a634d46df1c3268687771cd921a3c44048
PERed <danielredov123@gmail.com>
2026-06-08 12:31:27 +0300

    Обновление дорожной карты (#332)

--- Content.Shared/DeadSpace/Soyuz/Roadmap/RoadmapEntryPrototype.cs
+++ Content.Shared/DeadSpace/Soyuz/Roadmap/RoadmapEntryPrototype.cs
@@ -12,18 +12,35 @@ public sealed partial class RoadmapEntryPrototype : IPrototype
     [IdDataField]
     public string ID { get; private set; } = default!;
     
+    /// <summary>
+    /// Название карточка
+    /// </summary>
     [DataField("title", required: true)]
     public string Title { get; private set; } = string.Empty;
     
+    /// <summary>
+    /// Описание карточки. Короткое описание отображается до 30 символов, далее троеточие. Полное описание требуется раскрыть
+    /// </summary>
     [DataField("description", required: true)]
     public string Description { get; private set; } = string.Empty;
     
+    /// <summary>
+    /// Категория карточки, в которой карточка будет отображаться
+    /// Completed, InProgress, Planned
+    /// </summary>
     [DataField("category", required: true)]
     public RoadmapCategory Category { get; private set; } = RoadmapCategory.Planned;
     
+    /// <summary>
+    /// Порядок отображения карточек в колонке
+    /// Записи с меньшим значением отображаются выше
+    /// </summary>
     [DataField("order")]
     public int Order { get; private set; } = 0;
     
+    /// <summary>
+    /// Теги, отображаемые снизу карточки. Сортировки по тегам нет :(
+    /// </summary>
     [DataField("tags")]
     public List<string> Tags { get; private set; } = new();
 }
